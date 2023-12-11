489c7c4a9a2cbefffae514f61d446361fa1fe927
alina <alina.rashid2002@gmail.com>
2023-12-11 21:23:08 +0400

    made a css folder within a public folder  in recipe-generator and added the recipe.css file in the css folder. Changed the ingredients-recipe.js to have app.use to include public folder and changed the recipe.ejs to link css

--- ingredients-recipe.js
+++ ingredients-recipe.js
@@ -7,6 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
+app.use(express.static("public"));
 
 function fetchdata(ingredients) {
     const apiKey = 'c8fe12a4c0e0441596e2c6a1c0b5967a';
